c8d0d0e07402f575b991d367ddf32894f47d57d0
Slastenin Nikolay <slastenin13@gmail.com>
2021-07-31 00:25:53 +0300

    Completed the task

---
+++ js/script.js
@@ -0,0 +1,19 @@
+"use strict";
+// Создаю переменную с числом
+let num = 266219;
+
+//  Преобразую число в строку, далее в массив. Коныертирую строковые значения в числовые.
+let array = Array.from(String(num)).map((i) => Number(i));
+
+// Создаю цикл в котором перемножаю каждый элемент массива.
+let result = 1;
+for (let i = 0; i < array.length; i++) {
+    result = result * array[i];
+}
+
+// Возвожу полученный результат в степень 3
+result = result ** 3;
+
+// Преобразую результат в строку и вывожу в консоль два первых числа.
+result = String(result);
+console.log(Number(result[0]), Number(result[1]));
