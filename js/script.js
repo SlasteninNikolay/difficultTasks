ce9cfb92691cb55052893cf760bfd55ed72d6557
Slastenin Nikolay <slastenin13@gmail.com>
2021-08-03 22:56:25 +0300

    Completed the task

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 "use strict";
+let str = prompt("Введите строку", "  123456789A123456789B123456789C  ");
+let maxLength = 30;
+
+const checkString = function (str) {
+    if (typeof str !== "string") {
+        alert("Не строка!");
+    } else {
+        return str.trim().slice(0, maxLength) + "...";
+    }
+};
+
+let result = checkString(str);
+alert(result);
