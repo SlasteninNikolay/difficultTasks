8c96ac254cd316e6ad63f981d6517d9f6a21f49a
Slastenin Nikolay <slastenin13@gmail.com>
2021-08-07 17:02:13 +0300

    Completed the task

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
 "use strict";
+let week = [
+    "воскресенье",
+    "понедельник",
+    "вторник",
+    "среда",
+    "четверг",
+    "пятница",
+    "суббота",
+];
+
+let now = new Date();
+let today = week[now.getDay()];
+
+for (let i = 0; i < week.length; i++) {
+    if (week[i] === today) {
+        let red = week[i];
+        red = red.bold();
+        week[i] = red;
+    }
+    if (week[i] === "воскресенье" || week[i] === "суббота") {
+        let change = week[i];
+        change = change.italics();
+        week[i] = change;
+    }
+
+    document.write(week[i] + "</br>");
+}
